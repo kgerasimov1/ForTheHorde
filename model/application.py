0320eb9e8bda8ee17915de0b0987ce0173382d58
Alex <starovaleksej@yandex.ru>
2017-10-08 19:52:33 +0400

    Перемещение методов в  Application Manager.
    Шаг 7: перемещение вспомогательных методов из файла с тестами в Application Manager.

--- model/application.py
+++ model/application.py
@@ -1,4 +1,33 @@
 class Application(object):
-
 	def __init__(self,driver):
 		self.driver = driver
+
+	def go_to_home_page(self):
+		self.driver.get("http://localhost/php4dvd/")
+
+	def login(self, user):
+		driver = self.driver
+		driver.find_element_by_id("username").clear()
+		driver.find_element_by_id("username").send_keys(user.username)
+		driver.find_element_by_name("password").clear()
+		driver.find_element_by_name("password").send_keys(user.password)
+		driver.find_element_by_name("submit").click()
+
+	def logout(self):
+		driver = self.driver
+		driver.find_element_by_link_text("Log out").click()
+		driver.switch_to_alert().accept()
+
+	def add(self, user):
+		driver = self.driver
+		driver.find_element_by_link_text("Add movie").click()
+		driver.find_element_by_name("name").clear()
+		driver.find_element_by_name("name").send_keys(user.name)
+		driver.find_element_by_name("year").clear()
+		driver.find_element_by_name("year").send_keys(user.year)
+		driver.find_element_by_id("submit").click()
+
+	def remove(self):
+		driver = self.driver
+		driver.find_element_by_link_text("Remove").click()
+		driver.switch_to_alert().accept()
--- test_php4dvd.py
+++ test_php4dvd.py
@@ -3,43 +3,20 @@ from selenium import webdriver
 from selenium.common.exceptions import *
 from selenium_fixture import app
 
-def login(driver, user):
-    driver.find_element_by_id("username").clear()
-    driver.find_element_by_id("username").send_keys(user.username)
-    driver.find_element_by_name("password").clear()
-    driver.find_element_by_name("password").send_keys(user.password)
-    driver.find_element_by_name("submit").click()
-
-def logout(driver):
-    driver.find_element_by_link_text("Log out").click()
-    driver.switch_to_alert().accept()
-
-def add(driver, user):
-    driver.find_element_by_link_text("Add movie").click()
-    driver.find_element_by_name("name").clear()
-    driver.find_element_by_name("name").send_keys(user.name)
-    driver.find_element_by_name("year").clear()
-    driver.find_element_by_name("year").send_keys(user.year)
-    driver.find_element_by_id("submit").click()
-
-def remove(driver):
-    driver.find_element_by_link_text("Remove").click()
-    driver.switch_to_alert().accept()
-
 def test_login(app):
-    app.driver.get("http://localhost/php4dvd/")
-    login(app.driver, User.Admin())
-    logout(app.driver)
+    app.go_to_home_page()
+    app.login(User.Admin())
+    app.logout()
 
 def test_add_film(app):
-    app.driver.get("http://localhost/php4dvd/")
-    login(app.driver, User.Admin())
-    add(app.driver, User.AddFilm())
-    logout(app.driver)
+    app.go_to_home_page()
+    app.login(User.Admin())
+    app.add(User.AddFilm())
+    app.logout()
 
 def test_remove_film(app):
-    app.driver.get("http://localhost/php4dvd/")
-    login(app.driver, User.Admin())
-    add(app.driver, User.RemoveFilm())
-    remove(app.driver)
-    logout(app.driver)
+    app.go_to_home_page()
+    app.login(User.Admin())
+    app.add(User.RemoveFilm())
+    app.remove()
+    app.logout()
